c583a7dd712ddd2118037401336eebc4ad0bc503
Oliver Moloney <127998765+Scrapjon@users.noreply.github.com>
2025-08-08 13:56:31 +1000

    Add MNIST setup

--- ImageRecognition/__init__.py
+++ ImageRecognition/__init__.py
@@ -1,1 +1,7 @@
-import torchvision 
+from torchvision.datasets import MNIST
+from pathlib import Path
+from os import getcwd
+
+DATA_FOLDER = Path("ImageRecognition","data")
+
+dataset = MNIST(DATA_FOLDER, download=True)
